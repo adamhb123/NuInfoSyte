013b318e885594a1c0b41a44617b8be6f7799956
Adam Brewer <adamhb321@gmail.com>
2021-08-26 08:50:37 -0400

    Lazy

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # NuInfoSyte
+## Notes
+In the future I may remove the NuInfoSys submodule and simply
+make it an installable module. For now having it as a submodule
+makes development simpler.
--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 """Application Entrypoint
 Serves web + api
 """
-from flask import Flask, render_template, request, jsonify
+from flask import Flask
 import betabrite_middleware
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object("config")
 from routes import web, api
 
-if __name__=="__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
