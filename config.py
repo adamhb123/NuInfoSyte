84f2c68c1970f891800757641bb8362af54e3487
Adam Brewer <adamhb321@gmail.com>
2021-09-01 21:39:16 +0100

    lol

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 from distutils.util import strtobool
 # Flask Config
 DEBUG: bool = strtobool(os.environ.get("DEBUG", "False"))
-IP: str = os.environ.get("PACKET_IP", "localhost")
-PORT: str = os.environ.get("PACKET_PORT", "6969")
+IP: str = os.environ.get("NUINFOSYS_IP", "localhost")
+PORT: str = os.environ.get("NUINFOSYS_PORT", "6969")
 PROTOCOL: str = os.environ.get("NUINFOSYS_PROTOCOL", "https://")
 SERVER_NAME: str = os.environ.get("NUINFOSYS_SERVER_NAME", f"{IP}:{PORT}")
 
